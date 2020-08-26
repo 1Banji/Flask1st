c365eb1e8b1599a9b8fd8dc6bff5cd499af38e3c
Banji <olabanji.ajayi@gmail.com>
2020-08-26 11:04:53 +0100

    -m 'new templates'

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+post = [
+    {
+        'author' :'Banji Ajayi',
+        'title' : 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author' :'Jane Doe',
+        'title' : 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 30, 2020'
+    }
+]
+
 @app.route('/')
-def hello_world():
-    return '<h1>banji!</h1>'
+@app.route('/home')
+def home():
+    return render_template('home.html',posts =post)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-#if __name__ == '__main__':
- #   app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
